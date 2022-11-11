a8701a0b44073c0553de6745664fcb9a07d5dba7
Sabine.Eitel <Seit201@aol.com>
2022-11-11 10:30:40 +0100

    scope

--- script_01.js
+++ script_01.js
@@ -2,3 +2,26 @@
 
 /***** Funktionen 01 *****/
 
+// 1. Kapselung von Codeblöcken
+
+// Funktionsaufruf (call)
+// test();
+
+// Funktiionsrumpf (body | callee
+// Funktionsdeklraration)
+function test()
+{
+    console.log("Hallo Sabine!");
+}
+
+/****** Funktionen 02a ******/
+// 2a. Parametrisierung + Datenübergabe von INNEN
+
+// ausgabeName();
+// console.log(firstname);   // Fehler: Scope!
+
+function ausgabeName()
+{ let firstName = "Sabine"; // what happens in Vegas, stays in Vegas
+    console.log("Hallo " + firstName +"!");
+}
+
