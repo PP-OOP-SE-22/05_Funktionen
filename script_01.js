3e0177050b1759046031dc870f068794951d63f7
Sabine.Eitel <Seit201@aol.com>
2022-11-11 11:04:15 +0100

    calls/Parameter/Argumente

--- script_01.js
+++ script_01.js
@@ -7,7 +7,7 @@
 // Funktionsaufruf (call)
 // test();
 
-// Funktiionsrumpf (body | callee
+// Funktionsrumpf (body | callee
 // // Funktionsdeklraration)
 // function test()
 // {
@@ -24,9 +24,23 @@
 //     console.log("Hallo " + firstName +"!");
 // }
 
-    function ausgabeNameParam(firstName) // Parameter
-    {
-    console.log("Hello " + firstName +"!");
-    }
-    ausgabeNameParam("World!"); // Argument
+    // function ausgabeNameParam(firstName) // Parameter
+    // {
+    // console.log("Hello " + firstName +"!");
+    // }
+    // ausgabeNameParam("World!"); // Argument
     // Argumente sind Daten für Parameter
+
+
+    /********* Funktionen 02c ********/
+    // 2c. Mehrere Parameter
+
+    function ausgabeNameParam(firstName, familyName) // Parameter
+    {
+    console.log("Hello " + firstName + " " + familyName + "!"); //log=call
+    }
+    // ausgabeNameParam("Michael", "Müller");
+    ausgabeNameParam(prompt("Vorname?"),prompt("Nachname?"));
+
+    // Funktionen können auch mehrere Parameter haben z.B
+    // (firstname, familyName, Age...)
